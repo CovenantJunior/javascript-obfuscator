a963e3cf294811511fb0a82de1999ad17597e4b8
Tea <covenanttope@gmail.com>
2023-08-11 00:00:22 +0100

    space

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 "use strict";
-
 import { TDictionary } from './src/types/TDictionary';
 import { TInputOptions } from './src/types/options/TInputOptions';
 import { TObfuscationResultsObject } from './src/types/TObfuscationResultsObject';
